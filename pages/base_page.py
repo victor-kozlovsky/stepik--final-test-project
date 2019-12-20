0c40d3803fefcd075c8085c01d35204e2de169d5
victor-kozlovsky <victor.kozlovsky@gmail.com>
2019-12-20 20:32:19 +0300

    transfer of common methods to the BasePage -- login link

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -13,9 +14,6 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
-
-    def open(self):
-        self.browser.get(self.url)
 
     def is_element_present(self, how, what):
         try:
@@ -39,6 +37,16 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)  # ==LOGIN_LINK_INVALID
+        link.click()
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
--- test_product_page.py
+++ test_product_page.py
@@ -23,20 +23,6 @@ def test_gest_can_add_product_to_basket(browser, link):
     page.should_be_correct_product_added(name_product, price_product)
 
 
-def test_gest_should_be_btn_add_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_btn_add_to_basket()
-
-
-def test_gest_should_be_btn_write_review(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_btn_write_review()
-
-
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
@@ -60,3 +46,31 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_product_to_basket()
     page.success_message_disappeared()
+
+
+def test_gest_should_be_btn_add_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_btn_add_to_basket()
+
+
+def test_gest_should_be_btn_write_review(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_btn_write_review()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
