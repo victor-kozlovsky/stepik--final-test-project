cdfe3eb43f2318b5845472e7531da57a0fccf7ac
victor-kozlovsky <victor.kozlovsky@gmail.com>
2019-12-20 16:18:07 +0300

    Transitions between pages --LoginPage

---
+++ pages/locators.py
@@ -0,0 +1,14 @@
+
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_URL = "login"
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+
