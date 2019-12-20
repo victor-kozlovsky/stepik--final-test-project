b7582cfd78c07021140f1dd0bea5178ba260cf2d
victor-kozlovsky <victor.kozlovsky@gmail.com>
2019-12-20 16:10:58 +0300

    added PageObject and updated fist test

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language: es or fr")
 
 
@@ -15,7 +15,6 @@ def pytest_addoption(parser):
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     language = request.config.getoption("language")
-    print('\n', browser_name, language)
     browser = None
     if language is None:
         raise pytest.UsageError("--language should be es, fr, ..")
@@ -24,13 +23,11 @@ def browser(request):
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
         browser = webdriver.Chrome(options=options)
-
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", language)
         browser = webdriver.Firefox(firefox_profile=fp)
-
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
