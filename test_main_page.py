739c2af112e9febd507083938df18a255568cce9
victor-kozlovsky <victor.kozlovsky@gmail.com>
2019-12-20 16:14:17 +0300

    verification method added --is_element_present

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 
 from .pages.main_page import MainPage
 
+
 link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser): 
@@ -9,3 +10,7 @@ def test_guest_can_go_to_login_page(browser):
    page.go_to_login_page()
 
 
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
